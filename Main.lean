6c7e863bce119c04a5747baf9e8035e7323172d5
Phil de Joux <philderbeast@gmail.com>
2024-01-21 16:27:57 -0500

    Show DMS

--- Main.lean
+++ Main.lean
@@ -3,5 +3,7 @@ import OblateSpheroid
 def main : IO Unit := do
   let london := LatLng.mk 51.5007 (-0.1246)
   let newyork := LatLng.mk 40.6892 (-74.0445)
+  let dms := DMS.mk 90 12 0.999
   IO.println s!"London: {london}"
   IO.println s!"New York: {newyork}"
+  IO.println s!"DMS: {dms}"
