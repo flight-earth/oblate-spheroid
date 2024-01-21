30dad6d65dac728a23e99b695289c6392edbbe07
Phil de Joux <philderbeast@gmail.com>
2024-01-21 12:23:01 -0500

    Define structures for problems and solutions

--- Main.lean
+++ Main.lean
@@ -1,4 +1,7 @@
-import «OblateSpheroid»
+import OblateSpheroid
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+def main : IO Unit := do
+  let london := LatLng.mk 51.5007 (-0.1246)
+  let newyork := LatLng.mk 40.6892 (-74.0445)
+  IO.println s!"London: {london}"
+  IO.println s!"New York: {newyork}"
