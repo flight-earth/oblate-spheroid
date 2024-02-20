54cdf5e96f69d184f1a637f36a6e68f50a9ed99e
Phil de Joux <philderbeast@gmail.com>
2024-02-20 12:17:43 -0500

    Switch to v4.6.0-rc1

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,8 @@
 import Lake
 open Lake DSL
 
-require std from git "git@github.com:leanprover/std4/" @ "main"
+require std from git
+  "git@github.com:leanprover/std4/" @ "v4.6.0-rc1"
 
 package «oblate-spheroid» where
   -- add package configuration options here
