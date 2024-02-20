1da1d621614a7af8895e5f91e38057b85c80d20b
Phil de Joux <philderbeast@gmail.com>
2024-02-20 18:40:16 -0500

    Add show for the problems and solutions

--- Geodesy/Problems.lean
+++ Geodesy/Problems.lean
@@ -12,9 +12,6 @@ structure Az where
 structure Dist where
   dist : Float
 
-instance : ToString Dist where
-  toString d := d.dist.toStringFull
-
 structure DirectProblem where
   x : LatLng
   az1 : Az
@@ -32,3 +29,21 @@ structure InverseSolution where
   s : Dist
   az1 : Az
   az2 : Option Az
+
+instance : ToString Dist where
+  toString d := d.dist.toStringFull
+
+instance : ToString Az where
+  toString a := a.az.toStringFull
+
+instance : ToString DirectProblem where
+  toString p := s!"(x={p.x}, az1={p.az1}, s={p.s})"
+
+instance : ToString InverseProblem where
+  toString p := s!"(x={p.x}, y={p.y})"
+
+instance : ToString DirectSolution where
+  toString s := s!"(y={s.y}, az2={s.az2})"
+
+instance : ToString InverseSolution where
+  toString s := s!"(s={s.s}, az1={s.az1}, az2={s.az2})"
--- Main.lean
+++ Main.lean
@@ -20,9 +20,14 @@ def newyork : LatLng :=
   }
 
 def distanceLonNyc := distance london newyork
+def inverseLonNyc := inverse ⟨london, newyork⟩
 
 /-- info: 5574840.456848554313182830810546875 -/
 #guard_msgs in #eval distanceLonNyc
+/--
+info: (s=5574840.456848554313182830810546875, az1=-1.250757751225103131531568578793667256832122802734375, az2=(some 1))
+-/
+#guard_msgs in #eval inverseLonNyc
 
 def main : IO Unit := do
   let dms : DMS := ⟨90, 12, 0.999⟩
